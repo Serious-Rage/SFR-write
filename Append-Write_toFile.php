406ac6bb2ceb72c113fb8f70d26e8456c55ce963
Brandon McAlpine <hughmann.spirit@gmail.com>
2016-10-06 17:44:38 -0700

    update

--- Append-Write_toFile.php
+++ Append-Write_toFile.php
@@ -1,24 +1,17 @@
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 
 <html>
 <head>
-	<title>Write/Appnd File</title>
-	<link rel="stylesheet" type="text/css" href="css/basic.css" />
+	<title>&dagger;SFR-write&dagger;</title>
+	<link rel="stylesheet" type="text/css" href="css/basic_white.css" />
 </head>
 
 <body>
 
-<h3>Write to File</h3>
+<h1 class="centered">DATA WAS APPENDED TO THE TEXT DOCUMRNT FRAMED BELOW</h1> 
 
 <?php
-
-	$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
-
-	//************************************
-	//  Write to File  (Overwrite)
-	//************************************
-
-
 
         $Selected_File = $_POST[slctFile];
 
@@ -59,11 +52,7 @@
 	fclose($fp);
 
 
-	//************************************
-		//  Append to File  (Append)
-	//************************************
-
-	        print "<h4>Appending File<b/h4>>";
+	      //  print "<h4>Appending File<b/h4>>";
 
 	
                 $filename = 'txt/' . $Selected_File  . '_A1.txt';     // passing in an array the first argument being the file to append to
@@ -92,9 +81,9 @@
 
 		}
 
-print '<br><br><h2>Complete appended Document</h2><br>';
+//print '<br><br><h2>Complete appended Document</h2><br>';
 
-
+/*
                 $filename = 'txt/' . $Selected_File.'_A1.txt';  
 		$lines_in_file = count(file($filename));
 
@@ -105,11 +94,20 @@ print '<br><br><h2>Complete appended Document</h2><br>';
 			$line = fgets($fp);  //Reads one line from the file
 			$trimedLine = trim($line);
 
-			print '<h>line:'.$ii.' - '.$trimedLine.'</h><br />';
+			print '<h>'.$ii.' - '.$trimedLine.'</h><br/>';
 		}
-
+*/
 		fclose($fp);
 
+
+$filename = 'txt/' . $Selected_File  . '_A1.txt';  
+
+
+echo "<br/>";
+
+echo "<div id=\"containFrame\" style=\"background-color:#bababa\"><br/>";
+echo "<iframe id=\"SFR_110\" src=\"txt/" . $Selected_File  . "_A1.txt\" width=\"inherit\" height=\"300px\" scrolling=\"yes\" ></iframe>";	
+echo "</div>";
 
 ?>
 
